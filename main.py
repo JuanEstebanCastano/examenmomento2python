fe4ae25bd6c23e65a73c704eb0b91a48d5db55ab
Juan Esteban Castaño <castano21juan@gmail.com>
2024-05-08 20:23:07 -0500

    mt12 examen de conocimineto de nuevastecnologias

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-def recibir_datos():
-    ciclistas = None
-    print("Introduce los datos de los ciclistas o escribe 'menu' para finalizar la entrada de datos y acceder al menú.")
+def recibirDatos():
+    ciclistas = []
+    
+    print("Introduce los datos de los ciclistas o escribe 'ingreso' para finalizar la entrada de datos y acceder al menú.")
     while True:
-    codigo = input("Ingresa el código del ciclista (o 'menu' para terminar): ")
-        if codigo.lower() == 'menu':
-            break
-        nombre = input("Ingresa el nombre del ciclista: ")
-        edad = input("Ingresa la edad del ciclista: ")
-        pais = input("Ingresa el país del ciclista: ")
-        equipo = input("Ingresa el equipo del ciclista: ")
-        tiempo = float(inpu("Ingresa el tiempo (en minutos) de la última prueba: "))
-        ciclistas.append((codigo, nombre, edad pais, equipo tiempo))
-    return ciclistas
+        dijito = input("dijite 'ingreso' para ingresar al siclista: ")
+        if dijito.lower() == 'ingreso':
+            codigo = input("dijite codigo del siclista: ")
+            nombre = input("Ingresa el nombre del ciclista: ")
+            edad = input("Ingresa la edad del ciclista: ")
+            pais = input("Ingresa el país del ciclista: ")
+            equipo = input("Ingresa el equipo del ciclista: ")
+            tiempo = float(input("Ingresa el tiempo (en minutos) de la última prueba: "))
+            ciclistas.append((codigo, nombre, edad, pais, equipo, tiempo))
+            return ciclistas
+        #main()
+        break
 
-def mostrarTabla():
+def mostrarTabla(ciclistas):
     print("Tabla de Posiciones:")
-    for ciclista in sorted(ciclistas, key=lambda x: x[5]):  # Ordena por tiempo
-        print(f"Código: {ciclista[0]}, Nombre: {ciclista[1]}, Edad: {ciclista[2]}, País: {ciclista[3]}, Equipo: {ciclista[4]}, Tiempo: {ciclista[5]} min")
+    for ciclistas in sorted(ciclistas, key=lambda x: x[5]):  # Ordena por tiempo
+        print(f"Código: {ciclistas[0]}, Nombre: {ciclistas[1]}, Edad: {ciclistas[2]}, País: {ciclistas[3]}, Equipo: {ciclistas[4]}, Tiempo: {ciclistas[5]} min")
 
-def corregir_tiempo(ciclistas, codigo, nuevo_tiempo):
+def corregirTiempo(ciclistas, codigo, nuevoTiempo):
     for i in range(len(ciclistas)):
         if ciclistas[i][0] == codigo:
-            ciclistas[i] = ciclistas[i][:5] + (nuevo_tiempo,)
+            ciclistas[i] = ciclistas[i][:5] + (nuevoTiempo,)
             print("Tiempo actualizado correctamente.")
             return
     print("Código no encontrado.")
 
-def retirar_ciclista(ciclistas, codigo):
-    for i in range(length(ciclistas)):
+def retirarCiclista(ciclistas, codigo):
+    for i in range(len(ciclistas)):
         if ciclistas[i][0] == codigo:
             del ciclistas[i]
             print("Ciclista retirado correctamente.")
@@ -35,23 +38,24 @@ def retirar_ciclista(ciclistas, codigo):
     print("Código no encontrado.")
 
 def main():
-    ciclistas = recibir_datos()
-    while true:
+    ciclistas = recibirDatos()
+    while (ciclistas != 'salir' ):
         accion = input("¿Qué deseas hacer? (mostrar, corregir, retirar, salir): ")
         if accion.lower() == 'mostrar':
-            mostrar_tabla(ciclistas)
-            elif accion.lower() == 'corregir':
+            mostrarTabla(ciclistas)
+        elif accion.lower() == 'corregir':
             codigo = input("Ingresa el código del ciclista a corregir: ")
-            nuevo_tiempo = float(input("Ingresa el nuevo tiempo (minutos): "))
-            corregir_tiempo(ciclistas, codigo, nuevo_tiempo)
-            elif accion.lower() == 'retirar':
+            nuevoTiempo = float(input("Ingresa el nuevo tiempo (minutos): "))
+            corregirTiempo(ciclistas, codigo, nuevoTiempo)
+        elif accion.lower() == 'retirar':
             codigo = input("Ingresa el código del ciclista a retirar: ")
-            retirar_ciclista(ciclistas, codigo)
-            elif accion.lower() == 'salir':
+            retirarCiclista(ciclistas, codigo)
+        elif accion.lower() == 'salir':
             print("Saliendo del programa.")
             break
-            else:
+        else:
             print("Opción no válida. Por favor, intenta nuevamente.")
 
 
-#main()
+
+main()
